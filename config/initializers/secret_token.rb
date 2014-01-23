f1baa2da6804faf24450a4666df9d8fb3cfd75a0
Tony <tcao@chicagotechacademy.org>
2014-01-23 13:18:07 -0600

    No changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwilApp::Application.config.secret_key_base = 'e4a28be39865d10a50117995890bb5abee45d3ef12e20165ed666469bee73fbb9793ef3f1111c0a95a1eaeff790d5879da7ad892e8e07844d91011c9edfbf089'
